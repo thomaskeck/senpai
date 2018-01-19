c33fe71813a033448900c364201f7cdb529f96b6
Thomas Keck <thomas.keck2@kit.edu>
2018-01-19 17:35:40 +0100

    Added everything required for a python package

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from distutils.core import setup
 
 setup(name='senpai',
+      author='Thomas Keck',
+      author_email='t.keck@online.de',
+      url='https://github.com/thomaskeck/senpai',
+      keywords='RSA, cryptography, senpai, dating problem',
+      install_requires=['sympy', 'numpy'],
+      python_requires='>=3',
       version='0.1',
       packages=['senpai'],
      )
